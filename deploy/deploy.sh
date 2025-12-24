b4402a00cac09bd881d35298d2a27e04858dc865
Crazy Barbarian <kruglov.dot@yandex.ru>
2025-12-24 17:10:10 +0300

    Fixed nginx.conf

---
+++ deploy/deploy.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+# set -e
+
+# REPO_NAME="WebSolow"
+# REPO_DIR="$HOME/$REPO_NAME"
+# NGINX_SITES_AVAILABLE="/etc/nginx/sites-available"
+# NGINX_SITES_ENABLED="/etc/nginx/sites-enabled"
+
+# Since I've only started, deployment is manual
