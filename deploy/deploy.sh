7c3be5e86bf3ab4b6d68691aeeeea4b949e0b2dd
Crazy Barbarian <crazy-barbarian@yandex.ru>
2026-01-08 13:15:28 +0300

    fixed deploy.sh

--- deploy/deploy.sh
+++ deploy/deploy.sh
@@ -54,11 +54,11 @@ if [ -d "$REPO_DIR/frontend" ]; then
     cp -fr "$REPO_DIR/frontend/"* "$FRONTEND_DIR/WebSolow/"
 fi
 
-# create symlinks in sites-enabled
+# copy files to sites-enabled
 
-ln -s "$NGINX_SITES_AVAILABLE/"* "$NGINX_SITES_ENABLED/"
+cp -fr "$NGINX_SITES_AVAILABLE/" "$NGINX_SITES_ENABLED/"
 
 chown -R www-data:www-data "$FRONTEND_DIR"
 chmod -R 755 "$FRONTEND_DIR"
 
-echo "sudo nginx -t"
+echo "$(nginx -t)"
